b29dd7e9a5a453a6cfc72862fb8d8e73fe0e2b27
Chanhyeok Pakr <qkrcksgur999@gmail.com>
2024-05-10 16:57:57 +0900

    Applying EJS Template

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express');
 const router = express.Router();
 const rootDir = require('../util/path')
+const products = [];
 
 // GET request
 router.get('/add-product', (req, res, next) => {
@@ -10,8 +11,9 @@ router.get('/add-product', (req, res, next) => {
 
 // Post request
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body)
+    products.push({title: req.body.title});
     res.redirect('/');
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
