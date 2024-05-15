f1126beb1da7603e8c9cf779930d0108dd38575f
Chanhyeok Pakr <qkrcksgur999@gmail.com>
2024-05-15 13:37:10 +0900

    alternatve routing

--- models/product.js
+++ models/product.js
@@ -2,11 +2,13 @@ const { fileLoader } = require("ejs");
 const fs = require("fs");
 const path = require("path");
 const { get } = require("../routes/admin");
+const Cart = require("./cart");
+
 const p = path.join(
-    path.dirname(require.main.filename),
-    "data",
-    "products.json"
-  );
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
 
 const getProductsFromfile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
@@ -19,7 +21,8 @@ const getProductsFromfile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title,imageUrl,price,description) {
+  constructor(id, title, imageUrl, price, description) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.price = price;
@@ -27,15 +30,46 @@ module.exports = class Product {
   }
 
   save() {
+    getProductsFromfile((products) => {
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
+    });
+  }
+
+  static deleteById(id) {
     getProductsFromfile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+          Cart.deleteProduct(product.id, product.price);
+        }
       });
     });
   }
 
   static fetchAll(cb) {
     getProductsFromfile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromfile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
   }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,24 @@
 const path = require('path')
 const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/admin')
+const adminController = require('../controllers/admin')
 
 // GET request
-router.get('/add-product', productController.getAddProduct);
-
-// Post request
-router.post('/add-product', productController.postProduct);
+router.get('/add-product', adminController.getAddProduct);
 
 // 권한있는 product page 호출
-router.get('/products', productController.getProducts);
+router.get('/products', adminController.getProducts);
+
+// Post request
+router.post('/add-product', adminController.postAddProduct);
+
+//edit page route
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+//post edit request 
+router.post('/edit-product', adminController.postEditProduct);
+
+//delete request
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
